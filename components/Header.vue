7862ba475d881e3cfee04408c512ecd6bfd51621
Kerry Kidd <kerry@raspikidd.com>
2024-03-30 14:26:17 +0000

    Initial Website commit

---
+++ components/Header.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="bg-white">
+  <header class="absolute inset-x-0 top-0 z-50">
+    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">micro:mag</span>
+          <img class="h-8 w-auto" src="@/static/Logo.png" alt="Logo">
+        </a>
+      </div>
+      <div class="flex lg:hidden">
+        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+          <span class="sr-only">Open main menu</span>
+          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+        <NuxtLink to ="/Issues" class="text-md font-semibold leading-6 text-gray-900">
+            Back Issues
+        </NuxtLink>
+        <!--<NuxtLink to ="/Blog" class="text-md font-semibold leading-6 text-gray-900">
+            Blog
+        </NuxtLink> -->
+      </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <!--<a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>-->
+      </div>
+    </nav>
+    <!-- Mobile menu, show/hide based on menu open state. -->
+    <div class="absolute top-0 inset-x-0 z-10 p-2 transition transform origin-top-right md:hidden">
+          <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div class="pt-5 pb-6 px-5">
+              <div class="flex items-center justify-between">
+                <NuxtLink to="/">
+                  <img class="h-8 w-auto sm:h-10" src="@/static/Logo.png" alt="Logo">
+                </NuxtLink>
+                <div class="-mr-2">
+                  <button type="button" @click="isOpen = !isOpen" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span class="sr-only">Open menu</span>
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                  </button>
+                  
+                  <transition
+                    enter-active-class="duration-200 ease-out"
+                    enter-class="opacity-0 scale-95"
+                    enter-to-class="opacity-100 scale-100"
+                    leave-active-class="duration-100 ease-in"
+                    leave-class="opacity-100 scale-100"
+                    leave-to-class="opacity-0 scale-95"
+                  >
+                    <div v-show="isOpen" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
+                      <div class="rounded-md bg-white shadow-xs">
+                        <div class="mt-6">
+                          <nav class="grid gap-y-8">
+        
+                            <NuxtLink to="/Issues" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 p-3">
+                              Back Issues
+                            </NuxtLink>
+                            <!--<NuxtLink to="/Blog" class="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 p-3">
+                              Blog
+                            </NuxtLink> -->
+                          </nav>
+                        </div>
+                      </div>
+                    </div>
+                  </transition>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  </header>
+  </div> 
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Header",
+  data: () => ({
+    isOpen: false,
+})
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="relative bg-gray-50">
+    <Header />
+    <main class="lg:relative">
+      <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-30 lg:text-center">
+        <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+            <span class="block xl:inline" style="color:#A42530"> Making <br> Programming<br> Fun </span> <br>
+            <!--<span class="block xl:inline xl:text-5xl"> through physical computing</span> -->
+          </h1>
+          
+          <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+            We do so with the help of microcomputers which are the size of a credit card. 
+            When our students write code for the micro:bit and Raspberry Pi etc, they can see an immediate result, e.g LEDs light 
+            up or robots move about.
+          
+          </p>
+        
+          <div class="grid grid-cols-2 grid-rows-2 gap-2 place-items-auto h-30 mt-10 sm:ml-3">
+            <div class="rounded-md shadow sm:mt-0 sm:ml-3">
+              <NuxtLink to="/microbit" style ="background-color:#76B82A; color:#0E7452; font-weight: bold; font-size: 18px" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-base rounded-md text-black md:py-4 md:text-lg md:px-4">
+                micro:bit
+              </NuxtLink>
+            </div>
+          
+            <div class="rounded-md shadow sm:mt-0 sm:ml-3">
+              <NuxtLink to="/TutorialLayout" style ="background-color:#76B82A; color:#0E7452; font-weight: bold; font-size: 18px" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-base rounded-md text-black md:py-4 md:text-lg md:px-4">
+                Raspberry Pi
+              </NuxtLink> 
+            </div>
+        
+            <!-- <div class="rounded-md shadow sm:mt-0 sm:ml-3">
+              <router-link to="/ComputerRepair" style ="background-color:#76B82A;" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-base rounded-md text-black md:py-4 md:text-lg md:px-4">
+                Computer Repair
+              </router-link>
+            </div>
+        
+            <div class="rounded-md shadow sm:mt-0 sm:ml-3">
+              <router-link to="/SoftwareInstall" style ="background-color:#76B82A;" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-base rounded-md text-black md:py-4 md:text-lg md:px-4">
+                Software Install
+              </router-link>
+            </div> -->
+          </div>
+        </div>
+      </div>
+  
+      <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+        <img class="absolute inset-0 w-full h-full object-cover" src="/images/PhysicalComputing.jpg" alt="home page image">
+      </div>
+    </main>
+
+    <!-- Neswletter form -->
+    <div class="bg-white" style="background-color:#A1C4B5">
+      <div class="px-6 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
+        <div class="px-6 py-6 bg-teal-600 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center">
+          <div class="xl:w-0 xl:flex-1">
+            <h2 class="text-2xl font-bold tracking-tight sm:text-3xl" style ="color:#0E7452">
+              Sign up to get updates straight to your inbox
+            </h2>
+        
+            <p class="max-w-3xl mt-3 text-lg leading-6 font-bold" style="color:#A42530; text-size: 20px;">
+              Enter your email to be the first to know about the latest news
+            </p>
+          </div>
+      
+          <div class="mt-8 sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
+
+            <!--<form class="md:flex" name="Newsletter" netlify action="https://formspree.io/f/moqrdzdn" method="POST">-->
+            <form class="md:flex" name="Newsletter" netlify>
+              <label for="name" class="sr-only">Name</label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autocomplete="text"
+                required
+                class="w-full px-5 py-3 mr-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white"
+                placeholder="Enter your name"
+              />
+              <label for="emailAddress" class="sr-only">Email address</label>
+              <input
+                id="emailAddress"
+                name="emailAddress"
+                type="email"
+                autocomplete="email"
+                required
+                class="w-full px-5 py-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white"
+                placeholder="Enter your email"
+              />
+              <button
+                type="submit"
+                class="flex items-center justify-center w-full px-5 py-3 mt-3 text-base font-medium text-black border border-transparent rounded-md shadow hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
+                style ="background-color:#76B82A; color:#0E7452; font-weight: bold; font-size: 22px"
+              >
+                Sign up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import Header from "~/components/Header.vue";
+  import Footer from "~/components/Footer.vue";
+
+  export default Vue.extend({
+    
+    components: {
+      Header,
+      Footer
+    }
+  });
+</script>
+
+
