2b8478661b05d050754cdaf71063bded917caeb0
Kerry Kidd <kerry@raspikidd.com>
2024-04-06 13:07:45 +0100

    Updated magazine page text

--- pages/Magazines.vue
+++ pages/Magazines.vue
@@ -9,7 +9,7 @@
         <div class="px-6 pt-14 lg:px-8">
           <div class="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
             <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Magazine Issues</h2>
-            <p class="mt-6 text-lg leading-8 text-gray-300">Here you will find all the Past issues to download as PDF</p>
+            <p class="mt-6 text-lg leading-8 text-gray-300">Here you will find all the past issues to download as PDF</p>
           </div>
         </div>
   
