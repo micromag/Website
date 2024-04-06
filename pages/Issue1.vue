8e4a06b2e318e702f5032b8c14b9ba1cfaba18ef
Kerry Kidd <kerry@raspikidd.com>
2024-04-06 13:04:54 +0100

    Magazine Issue 1 Added

--- pages/Issue1.vue
+++ pages/Issue1.vue
@@ -5,7 +5,8 @@
         <div class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div class="lg:pr-4">
             <div class="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-58 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
-              <img class="aspect-[14/13] w-full rounded-2xl object-cover" src="/Issue7.png" alt="Issue 7 Cover image" />
+              <!-- Magazine Issue Cover -->
+              <img class="aspect-[14/13] w-full rounded-2xl object-cover" src="/Issue1.png" alt="Issue 1 Cover image" />
               <div class="absolute left-1/2 top-1/2 -ml-16 -translate-x-1/2 -translate-y-1/2 transform-gpu blur-3xl" aria-hidden="true">
                 <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-40" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
               </div>
@@ -13,29 +14,26 @@
           </div>
           <div>
             <div class="text-base leading-7 text-gray-700 lg:max-w-lg">
-              <h1 class="mt-2 text-3xl font-bold tracking-tight text-blue-400 sm:text-4xl"> Issue 7 - 5 Awesome micro:bit Features </h1>
-              <p class="text-base font-semibold leading-7 text-white"> March 2020 </p>
+              <h1 class="mt-2 text-3xl font-bold tracking-tight text-blue-400 sm:text-4xl"> Issue 1 - micro:bits in Libraries </h1>
+              <p class="text-base font-semibold leading-7 text-white"> July 2018 </p>
               <div class="max-w-xl">
-                <p class="mt-6 text-blue-400 font-bold"> 5 Awesome micro:bit Features </p>
-                <p class="mt-6 text-white">This issue’s cover feature is “5 Awesome micro:bit Features”. If you’re a regular reader of micro:mag, you 
-                  probably already know that the BBC micro:bit is AWESOME. However, it’s easy to get caught up in using accessories such as robots, 
-                  speakers and LED lights and forget about some of the amazing onboard features the micro:bit has. That’s why this issue we have compiled 
-                  five of our favourite features of the BBC micro:bit and put them into this cover feature with code examples in three of the major code 
-                  editors for the micro:bit (MakeCode, EduBlocks and Python) which should cater for all levels of coders.</p>
+                <p class="mt-6 text-blue-400 font-bold"> micro:bit in Libraries </p>
+                <p class="mt-6 text-white">In this issues cover article, Michael Rimicans is giving us some insight into the micro:bit in libraries scheme. 
+                  You can now loan out micro:bits in hundreds of UK libraries, giving more people the chance to get hands on with computer science.</p>
                 <p class="mt-8 text-blue-400 font-bold">Plus!</p>
                 <p class="mt-6 text-white">
                     <ul>
-                        <li> Scratch Audio Game </li>
-                        <li> MIT App Inventor + micro:bit </li>
-                        <li> Adafruit Clue </li>
-                        <li> micro:bit Classroom Tool released </li>
+                        <li> Skulls or Snakes? </li>
+                        <li> Minecraft Pi and micro:bit </li>
+                        <li> Reviews </li>
+                        <li> Get to know the Micro:bit Educational Foundation </li>
                         <li> And more! </li>
                     </ul>
                 </p>
               </div>
             </div>
             <div class="mt-10 flex">
-                <a href="https://drive.google.com/file/d/1ltRolUWRvmlqmivRMfPkoeHTiRqxq7Y7/view?usp=sharing" target="_Blank" class="rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold text-grey-900 shadow-sm hover:bg-blue-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"> Download PDF </a>
+                <a href="https://drive.google.com/file/d/1u_A2zWtt4FQTuaJyLTX1ThbXQ8X3_1KO/view?usp=sharing" target="_Blank" class="rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold text-grey-900 shadow-sm hover:bg-blue-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"> Download PDF </a>
             </div>
           </div>
         </div>
