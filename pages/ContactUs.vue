11a33a2c042da02c2436e4c95a724f357ed96016
Kerry Kidd <kerry@raspikidd.com>
2024-04-21 13:15:26 +0100

    Another form test

--- pages/ContactUs.vue
+++ pages/ContactUs.vue
@@ -79,35 +79,35 @@
                     </div>
                 </form> -->
                 <form class="md:flex" name="Newsletter" method="POST" netlify>
-              <input type="hidden" name="form-name" value="Newsletter" />
-              <label for="name" class="sr-only">Name</label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                autocomplete="text"
-                required
-                class="w-full px-5 py-3 mr-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white"
-                placeholder="Enter your name"
-              />
-              <label for="emailAddress" class="sr-only">Email address</label>
-              <input
-                id="emailAddress"
-                name="emailAddress"
-                type="email"
-                autocomplete="email"
-                required
-                class="w-full px-5 py-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white"
-                placeholder="Enter your email"
-              />
-              <button
-                type="submit"
-                class="flex items-center justify-center w-full px-5 py-3 mt-3 text-base font-medium text-black border border-transparent rounded-md shadow hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
-                style ="background-color:#76B82A; color:#0E7452; font-weight: bold; font-size: 22px"
-              >
-                Sign up
-              </button>
-            </form>
+                    <input type="hidden" name="form-name" value="Newsletter" />
+                    <label for="name" class="sr-only">Name</label>
+                    <input
+                        id="name"
+                        name="name"
+                        type="text"
+                        autocomplete="text"
+                        required
+                        class="w-full px-5 py-3 mr-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white"
+                        placeholder="Enter your name"
+                    />
+                    <label for="emailAddress" class="sr-only">Email address</label>
+                    <input
+                        id="emailAddress"
+                        name="emailAddress"
+                        type="email"
+                        autocomplete="email"
+                        required
+                        class="w-full px-5 py-3 placeholder-gray-500 border-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white"
+                        placeholder="Enter your email"
+                    />
+                    <button
+                        type="submit"
+                        class="flex items-center justify-center w-full px-5 py-3 mt-3 text-base font-medium text-black border border-transparent rounded-md shadow hover:bg-teal-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-teal-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
+                        style ="background-color:#76B82A; color:#0E7452; font-weight: bold; font-size: 22px"
+                    >
+                        Sign up
+                    </button>
+                </form>
             </div>
         </div>
         <Footer />
